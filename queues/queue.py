e96879be61ca8c5f6eb9481ccef2439438cbe1d5
unknown <rmtestard@gmail.com>
2020-02-09 18:56:05 +0100

    I have implemented a queue in python

---
+++ queues/queue.py
@@ -0,0 +1,29 @@
+# A queue is an ordered collection of items where the addition of new items happens at one end, called the "rear", and the removal of existing items occurs at the other en commonly called the "front". FIFO principle
+
+class Queue(object):
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+""" q = Queue()
+print(q.isEmpty())
+q.enqueue(1)
+q.enqueue('A')
+print(q.isEmpty())
+print(q.size())
+print(q.peek()) """
